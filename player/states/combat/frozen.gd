a29a561b66c26c335d5167a56532614197036b86
SenorMooMoo <35905949+SenorMooMoo@users.noreply.github.com>
2022-07-14 22:00:52 -0400

    Added todo to frozen state

--- player/states/combat/frozen.gd
+++ player/states/combat/frozen.gd
@@ -1,16 +1,13 @@
 extends "res://state_machine/state.gd"
+"""
+Goal: Stop all movements for a given amount of time
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
+Todo:
+	1. Recieve input from transition
+	2. Pause movement
+	3. Add vizual effect
+	4. Write unit test
+"""
 func _ready():
+	#add some code
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
